7798b292d9bb2cd24aa7c295d18627712c40ffdb
Julian Chan <julian.chan@bateswhite.com>
2019-04-09 14:34:34 -0400

    testing hello.py

--- Tests/test_1.py
+++ Tests/test_1.py
@@ -1,4 +1,12 @@
 import pytest
+from src.hello import hello
+import sys
 
 def test_1():
     assert 1 == 1
+
+
+def test_hello(capsys):
+    hello()
+    captured = capsys.readouterr()
+    assert captured.out == "Hello World!\n"
